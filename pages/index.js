21a0ccb0b9e3bccfbc1d24098aaf7f26fdc19201
Stefano Lima <stefanocblima@gmail.com>
2023-06-26 11:51:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return(
-        <h1>Oi, Nandinha! Sabia que seu nome significa “ousado para atingir a paz”, “o que ousa viajar”, “viajante corajoso”?</h1>
-    )
+  return <h1>Em construção</h1>;
 }
 
 export default Home;
